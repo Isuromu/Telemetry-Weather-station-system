ecc81d51dba1ebdc6256bfba907ced1251090f7a
Islom <islomiut@gmail.com>
2026-03-12 17:02:15 +0500

    leaf sensor update

--- examples/JXBS_LeafSurfaceHumidity_Example/src/config.h
+++ examples/JXBS_LeafSurfaceHumidity_Example/src/config.h
@@ -8,10 +8,10 @@
 */
 
 #define SENSOR_ID         "jxbs_leaf_00"
-#define SENSOR_ADDRESS    0x01
+#define SENSOR_ADDRESS    0x06
 #define SENSOR_DEBUG      true
 
 #define NEW_ADDRESS       0x02
-#define DO_SCAN           true
+#define DO_SCAN           false
 #define TRY_ADDRESS_CHANGE_AT_BOOT false
 #define POLL_INTERVAL_MS  2000
--- examples/LeafSurfaceHumidity_Example/src/config.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define DEBUG_ENABLED 1
-#define DEBUG_LEVEL 2
